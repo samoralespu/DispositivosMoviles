bb48f0876440ca81f2f89b8a3b1ae0e5ce06d45a
Santiago Morales <santiagoalejandro.morales@csgi.com>
2021-10-13 18:04:15 -0500

    Hello world on flutter

---
+++ taller1/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      const Center(
+          child: Text(
+              "Hello world on flutter", textDirection: TextDirection.ltr
+          )
+      )
+  );
+}
